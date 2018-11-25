334b5a38ea055c356b044217fe24212d4efb5314
MiquelPuig <mpuig22@gmail.com>
2018-11-25 20:07:59 +0100

    Versión mejorada maquina de estados

--- src/control.cpp
+++ src/control.cpp
@@ -11,7 +11,11 @@
 #include <stdbool.h>
 #include "geometry_msgs/Vector3.h"
 #include "geometry_msgs/Twist.h"
+#include <unistd.h>
+
 //Estados
+const int TRUE = 1;
+const int FALSE	=0;
 const int STARTED	=0;
 const int STOPPED	=1;
 const int	BALL_APROX =2;
@@ -21,10 +25,14 @@ const int LOST =5;
 const int HOME_RETURN =6;
 int STATE=0;
 int LAST_STATE=0;
+int CHANGE_STATE=0;
 
 //variables de aproximación a pelota
-const double factorX=0.0004;
-const double factorA=0.0008;
+const double factorX=0.0004; //Factor de escalado direccional.
+const double factorA=0.0008; //Factor de escalado angular
+const double PRECISION=3; //precisión que se ha de cumplir para recojer la pelota. Mas pequeño mas precisión
+const double MINIUM_X=0.003; // Mínimo factor de movimiento en X
+const double MINIUM_Z=0.0060; // Mínimo factor de movimiento angular
 geometry_msgs::Vector3 aprox_vector;
 geometry_msgs::Twist movetoball;
 
@@ -51,12 +59,7 @@ MoveBaseClient ac("move_base", true);
         ROS_INFO("Waiting for the move_base action server to come up");
       }
       ac.sendGoal(goals);
-      /*ac.waitForResult();
-      if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED){
-           return true;}
-      else{
-           return false;}*/
-      //ROS_INFO("Goal enviado");
+
       return true;
 }
 
@@ -103,65 +106,134 @@ void chatterCallback(const geometry_msgs::PoseWithCovarianceStamped vector)
 
 void ballCallback(const geometry_msgs::Vector3& vector){
 
-ROS_INFO("Pelotas?");
+
 aprox_vector=vector;
   if (vector.z>0){
-    LAST_STATE=STATE;
+
     aprox_vector=vector;
-    STATE=BALL_APROX;
-  }else{
-    if(STATE==BALL_APROX){
+      //ROS_INFO("estado: %d",STATE);
+      if(STATE==EXPLORING||STATE==STARTED){
+        //ROS_INFO("dentro: %d",STATE);
+        LAST_STATE=STATE;
+        STATE=BALL_APROX;
+        CHANGE_STATE=TRUE;
+      }
+  }else if(STATE==BALL_APROX){
+
       STATE=LAST_STATE;
-    }
+      CHANGE_STATE=TRUE;
   }
 }
-
-
-
 
 int main(int argc, char **argv)
 {
 
   ros::init(argc, argv, "control");
 
-  //tell the action client that we want to spin a thread by default
-/*MoveBaseClient ac("move_base", true);
-
-  while(!ac.waitForServer(ros::Duration(5.0))){
-    ROS_INFO("Waiting for the move_base action server to come up");
-  }*/
-
-
   //declaración Subscriber
   ros::NodeHandle n;
   ros::Subscriber sub = n.subscribe("amcl_pose", 10,chatterCallback);
   ros::Subscriber dir = n.subscribe("/camera/direction", 10,ballCallback);
-  //declaración Publisher
-  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("state", 1000);
-  ros::Publisher ball_aprox = n.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/teleop", 1000);
-
-
+  //declaración Publisher STATE
   std_msgs::String msg;
   std::stringstream ss;
-  ss << "hello world ";
-  msg.data = ss.str();
+  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("state", 1000);
+  //declaración Publisher TELEOP
+  ros::Publisher ball_aprox = n.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/teleop", 1000);
+
   ros::Rate loop_rate(10);
 
 
   while (ros::ok())
   {
 //ROS_INFO("looop");
+
+
+//------------------------------
 //BALL_APROX
+//------------------------------
 if(STATE==BALL_APROX){
-  ROS_INFO("I like balls");
+  //ROS_INFO("I like balls");
+  //ROS_INFO("1 - Canvi estat a BALL_RECOLECT? %d",STATE );
+  double xb = aprox_vector.y*factorX;
+  double zb = -aprox_vector.x*factorA;
+  ROS_INFO ("INI xb: %lf  zb: %lf", xb,zb);
+  if(fabs(xb)<MINIUM_X){
+    //ROS_INFO ("MID abs(xb): %lf  MINIUM_X: %lf", abs(xb),MINIUM_X);
+    ROS_INFO("Minimo X");
+    if(xb>0){
+        ROS_INFO("Minimo X +");
+        xb=MINIUM_X;
+    }else if(xb<0){
+      ROS_INFO("Minimo X -");
+      xb=-MINIUM_X;
+    }
+  }
 
-  movetoball.linear.x =aprox_vector.y*factorX;
-  movetoball.angular.z=-aprox_vector.x*factorA;
+  if(fabs(zb)<MINIUM_Z){
+      ROS_INFO("Minimo Z");
+    if(zb>0){
+        ROS_INFO("Minimo Z +");
+      zb=MINIUM_Z;
+    }else if(zb<0){
+      ROS_INFO("Minimo Z -");
+      zb=-MINIUM_Z;
+    }
+  }
+  ROS_INFO ("FIN xb: %lf  zb: %lf", xb,zb);
 
+  movetoball.linear.x =xb;
+  movetoball.angular.z=zb;
 
-ball_aprox.publish(movetoball);
+  ball_aprox.publish(movetoball);
+
+  if(abs(aprox_vector.y)<=PRECISION && abs(aprox_vector.x)<=PRECISION){
+    STATE=BALL_RECOLECT;
+    CHANGE_STATE=TRUE;
+    //ROS_INFO("2 - Canvi estat a BALL_RECOLECT? %d",STATE );
+  }
 }
-    chatter_pub.publish(msg);
+
+//------------------------------
+//BALL_RECOLECT
+//------------------------------
+else if(STATE==BALL_RECOLECT){
+ROS_INFO("Simulación de Recolectar pelota: 5 segundos");
+usleep(3000000);
+STATE=LAST_STATE;
+CHANGE_STATE=TRUE;
+ROS_INFO("Volviendo al estado anterior %d", STATE);
+}
+//------------------------------
+//Enviar el último estado
+//------------------------------
+if(CHANGE_STATE==TRUE){
+  if(STATE==STARTED){
+      msg.data = "STARTED";
+   chatter_pub.publish(msg);
+  }else if(STATE==STOPPED){
+    msg.data = "STOPPED";
+   chatter_pub.publish(msg);
+  }else if(STATE==BALL_APROX){
+    msg.data =  "BALL_APROX";
+   chatter_pub.publish(msg);
+  }else if(STATE==BALL_RECOLECT){
+   msg.data = "BALL_RECOLECT";
+   chatter_pub.publish(msg);
+  }else if(STATE==EXPLORING){
+      msg.data =  "EXPLORING";
+     chatter_pub.publish(msg);
+  }else if(STATE==LOST){
+      msg.data =  "LOST";
+     chatter_pub.publish(msg);
+  }else if(STATE==HOME_RETURN){
+        msg.data =  "HOME_RETURN";
+       chatter_pub.publish(msg);
+  }
+
+  CHANGE_STATE=FALSE;
+}
+
 
     //ROS_INFO("%s", msg.data.c_str());
 
