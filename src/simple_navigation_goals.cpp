4037abd30f28c68b18e7578d94d94741604438ec
MiquelPuig <mpuig22@gmail.com>
2018-11-27 01:02:23 +0100

    Exploration funciona - falta integrar

--- src/simple_navigation_goals.cpp
+++ src/simple_navigation_goals.cpp
@@ -9,18 +9,20 @@
 #include "geometry_msgs/Pose.h"
 #include "geometry_msgs/Point.h"
 #include "nav_msgs/OccupancyGrid.h"
+//#include <Eigen/Eigen>
 
+//Eigen::MatrixXd eMap;
 
-const double GOAL4METER=0.5; //Goals por cada metro real de distancia.
+const double GOAL4METER=2; //Goals por cada metro real de distancia.
 float map_resolution = 0;
 int pixel_x = 0; //[pixeles de ancgura]
 int pixel_y = 0; //[pixeles de altura]
 double resolution =0; //Resolución del mapa [m/cell]
 geometry_msgs::Point origin;
-int PIXEL4GOAL=10;
+int PIXEL4GOAL=0;
+
 int nrows = 50;
 int ncols =4;
-
 
 int determine_size()
 {
@@ -87,29 +89,109 @@ typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseCl
  				grid = ros::topic::waitForMessage<nav_msgs::OccupancyGrid>("/map",n);
 
  				if(sharedEdge != NULL){
- 					ROS_INFO("Datos del mapa leidos2");
+
  					 mapaM = *grid;
 
  				 }
+				 ROS_INFO("Llegamos");
 
-				 //cargamos el mapa en una matriz para poder trabajar mejor con el
-				 int** mapa =new int*[pixel_x];
+
+
+         //cargamos el mapa en una matriz para poder trabajar mejor con él
+         /*int** mapa = (int **)malloc(pixel_x*pixel_y*sizeof(int));
+         //int** mapa =new int*[pixel_x];
 				 for(int i; i<pixel_x;i++){
  					mapa[i] = new int[pixel_y];
+						ROS_INFO("Fila consolidada");
 							for(int j;j<pixel_y;j++){
+
 									mapa[i][j]=(int)mapaM.data[i+j*pixel_x];
 							}
- 				  }
+ 				  }*/
+
+
 					//definimos PIXEL4GOAL
-					PIXEL4GOAL= (int)(GOAL4METER/resolution);
-					ROS_INFO("Pixels/GOAL: %d",PIXEL4GOAL);
-					//definimos una matriz con los Goals
+          //-------------------------------------
+					PIXEL4GOAL= (long)(1/(GOAL4METER*resolution));
+          ROS_INFO("PIXEL4GOAL %d  ",PIXEL4GOAL);
+          ROS_INFO("Resolution %lf",resolution);
+					//definimos un grid de exploración
+          //-------------------------------------
+			   	int grid_x	=	(int)(pixel_x/PIXEL4GOAL);
+					int grid_y =(int)(pixel_y/PIXEL4GOAL);
+          ROS_INFO("grid_x: %d", grid_x);
+          ROS_INFO("grid_y: %d", grid_y);
+
+          //Se tratará de una matriz con
+          int *exploreGrid = (int *) malloc(grid_x * grid_y * sizeof(int));
+
+          for(unsigned int i=0; i<grid_x;i++){
+              for(unsigned int j=0; j<grid_y;j++){
+                  exploreGrid[i+j*grid_x]=(int)mapaM.data[i*PIXEL4GOAL+j*pixel_x*PIXEL4GOAL];
+                  //ROS_INFO("Valor %d en: %d", exploreGrid[i+j*grid_x], i*PIXEL4GOAL+j*pixel_x*PIXEL4GOAL );
+              }
+          }
+          //Creamos la lista de goals budcando zeros en el GRID
+          //Pendiente hacerlo mas inteligente
+          //-------------------------------------
+          ROS_INFO("Tenim un GRID de %d punts", grid_x*grid_y);
+          double *goals = (double *) malloc(grid_x * grid_y *4 *sizeof(double));
+          double zpos= 0;
+          double wpos= 1;
+          int numGoals=0;
+          for(unsigned int i=0; i<grid_x;i++){
+              //añadir cambiar xpos y wpos
+              for(unsigned int j=0; j<grid_y;j++){
+                //añadir cambiar xpos y wpos
+                  if(exploreGrid[i+j*grid_x]==0){
+                      //ROS_INFO("Entro");
+                      //goals[numGoals*4]=(double)i/(double)GOAL4METER+(double)origin.x; //asignamos la X
+                      //goals[numGoals*4+1]=(double)j/(double)GOAL4METER+ (double)origin.y;
+                      goals[numGoals*4]= (double)i*(double)PIXEL4GOAL*resolution+ (double)origin.x;//asignamos la X
+                      goals[numGoals*4+1]=(double)j*(double)PIXEL4GOAL*resolution+(double)origin.y;
+                      goals[numGoals*4+2]=zpos;
+                      goals[numGoals*4+3]=wpos;
+                      numGoals++;
+                      //ROS_INFO("GOAL %d %lf %lf %lf %lf",numGoals, (double)i/(double)GOAL4METER,(double)i/(double)GOAL4METER,zpos,wpos);
+                  }
+
+              }
+          }
 
 
-				 /*for(int i; i<pixel_x*pixel_y; i++){
-					 	mapa[i]=(int)mapaM.data[i];
-						ROS_INFO("posició %d valod %d - ",i,mapa[i]);
-				 }*/
+           //Código que genera que supera el límite de memoria asignada.
+          /*int** exploredGrid =(int **)malloc(grid_x*grid_y*sizeof(int));
+					//int** exploredGrid =new int*[grid_x];
+					ROS_INFO("grid_x %d , grid_y %d", grid_x,grid_y);
+					for(int k; k<grid_x;k++){
+            //ROS_INFO("Entro");
+					 	exploredGrid[k] = new int[grid_y];
+							 for(int l;l<grid_y;l++){
+                    ROS_INFO("Entro %d", l);
+								 		//revisamos si el punto es visitable
+									 //exploredGrid[k][l]=mapa[k*PIXEL4GOAL][l*PIXEL4GOAL];
+									 //exploredGrid[k][l]=0;
+                   //ROS_INFO("Grid - (X: %d - Y: %d )",k,l);
+									 //ROS_INFO("Grid - (X: %d - Y: %d ): %d",k,l, exploredGrid[k][l]);
+
+							 }
+					 }*/
+
+					 //Creamos la lista de goals
+					 //possibles goals, aunqué realmente menos pues hay zonas no exploradas y ocupadas.
+
+					 /*
+					 int goalsnumber=grid_x*grid_y;
+					 double** goalslist=new double*[goalsnumber];
+					 for(int z; z<grid_x;z++){
+ 					 					goalslist[z] = new double[4];
+										goalslist[z][0]=0;
+										goalslist[z][1]=0;
+										goalslist[z][2]=0;
+										goalslist[z][3]=0;
+
+
+ 					 }*/
 
 
 
@@ -126,15 +208,10 @@ typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseCl
 
 				//Create points matrix
 
-				int** matrix = new int*[rows];
-
-
-
-
-
 
 
         //tell the action client that we want to spin a thread by default
+
         MoveBaseClient ac("move_base", true);
 
         //wait for the action server to come up
@@ -146,21 +223,21 @@ typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseCl
 
         goal.target_pose.header.frame_id = "/map";
         goal.target_pose.header.stamp = ros::Time::now();
-        for( int i = 0; i < lenght0f_Array; i = i + 1 ) {
+        for( int i = 0; i < numGoals; i = i + 1 ) {
 
-          goal.target_pose.pose.position.x = x_pos[i];
-          goal.target_pose.pose.position.y = y_pos[i];
-          goal.target_pose.pose.orientation.w = w_pos[i];
-	  goal.target_pose.pose.orientation.z = z_pos[i];
-            ROS_INFO("Sending goal");
-	    ROS_INFO("Posicion %d%d%d%d", x_pos[i],y_pos[i], z_pos[i], w_pos[i]);
+        goal.target_pose.pose.position.x =goals[i*4]; //(double)origin.x
+        goal.target_pose.pose.position.y =goals[i*4+1];//(double)origin.y
+        goal.target_pose.pose.orientation.w =goals[i*4+2];
+        goal.target_pose.pose.orientation.z =goals[i*4+3];
+          ROS_INFO("Sending goal");
+	         ROS_INFO("Posicion %lf %lf %lf %lf", goals[i*4],goals[i*4+1], goals[i*4+2], goals[i*4+3]);
           ac.sendGoal(goal);
 
           ac.waitForResult();
 
          if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
             ROS_INFO("Goal aceptado, moviendose" );
-	else
+	           else
             ROS_INFO("Error, goal inaceptable");
          }
 
